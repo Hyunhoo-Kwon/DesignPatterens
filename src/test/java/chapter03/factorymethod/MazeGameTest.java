6baf0438ac5a8e4f1b89a1747cdfb01377e37777
elon.kwon <elon.kwon@kakaocorp.com>
2019-02-17 01:05:56 +0900

    factory method 테스트 작성.

--- src/test/java/chapter03/factorymethod/MazeGameTest.java
+++ src/test/java/chapter03/factorymethod/MazeGameTest.java
@@ -1,13 +1,41 @@
 package chapter03.factorymethod;
 
+import chapter03.intro.domain.Maze;
+import chapter03.intro.domain.Room;
 import org.junit.Test;
 
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.*;
 
 public class MazeGameTest {
 
+    private MazeGame mazeGame;
+
     @Test
-    public void createMaze() throws Exception {
+    public void createDefaultMaze() throws Exception {
+        mazeGame = new DefaultMazeGame();
+        Maze maze = mazeGame.createMaze();
+
+        assertThat(maze, instanceOf(Maze.class));
+        assertThat(maze.roomNo(1), instanceOf(Room.class));
+    }
+
+    @Test
+    public void createEnchantedMaze() throws Exception {
+        mazeGame = new EnchantedMazeGame();
+        Maze maze = mazeGame.createMaze();
+
+        assertThat(maze, instanceOf(Maze.class));
+        assertThat(maze.roomNo(1), instanceOf(Room.class));
+    }
+
+    @Test
+    public void createBombedMaze() throws Exception {
+        mazeGame = new BombedMazeGame();
+        Maze maze = mazeGame.createMaze();
+
+        assertThat(maze, instanceOf(Maze.class));
+        assertThat(maze.roomNo(1), instanceOf(Room.class));
     }
 
 }
